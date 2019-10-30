402ce6a776239af60e2113e9b2ce85f5fa695262
ahmed GHOUL <ghoul.ahmed5@gmail.com>
2019-10-30 22:30:01 +0000

    example with content list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo';
+  list = [
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+  ];
+
+  items = [];
+  loader: boolean  = false;
+
+  refresh() {
+    this.items = [];
+    this.loader =  true;
+    setTimeout(() => {
+      this.items = this.list;
+       this.loader =  false;
+    }, 2000)
+
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ContentLoaderModule } from '@ngneat/content-loader';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContentLoaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
